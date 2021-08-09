dde7e41fa786a68f8f07df035007395fdb3a8efb
Krittipong Wachirangkul <34181035+newlesk@users.noreply.github.com>
2021-08-09 16:35:34 +0700

    update login

--- App.js
+++ App.js
@@ -1,39 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { auth } from './firebase';
+import LoginScreen from './screens/sessions/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [signedIn, setSignedIn] = useState(false);
-  
   auth.onAuthStateChanged((user) => {
     if (user) {
-    setSignedIn(true);
+      setSignedIn(true);
     } else {
-    setSignedIn(false);
+      setSignedIn(false);
     }
-    });
-
-    return (
-      <View style={styles.container}>
-        {signedIn
-          ? (
-            <Text>Signed in</Text>
-          ) : (
-            <Text>Not signed in</Text>
-          )
-        } 
-        </View>
-      );
-      
+  });
+  
+  return (
+    <NavigationContainer theme={DefaultTheme}>
+      {signedIn
+        ? (
+          <Text>Signed in</Text>
+        ) : (
+          <>
+            <StatusBar style="light" />
+            <Stack.Navigator
+              mode="card"
+              screenOptions={{
+              }}
+            >
+              <Stack.Screen
+                name="signIn"
+                component={LoginScreen}
+                options={{
+                  title: 'Sign in',
+                  headerStyle: {
+                    backgroundColor: '#29434e',
+                    borderBottomColor: '#29434e',
+                  },
+                  headerTintColor: '#fff',
+                }}
+              />
+            </Stack.Navigator>
+          </>
+        )}
+    </NavigationContainer>
+  );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
