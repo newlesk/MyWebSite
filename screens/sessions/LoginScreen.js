d06e0a2132256fd3ac7df6273fd810eb6a840917
Krittipong Wachirangkul <34181035+newlesk@users.noreply.github.com>
2021-08-16 17:52:44 +0700

    up

--- screens/sessions/LoginScreen.js
+++ screens/sessions/LoginScreen.js
@@ -1,13 +1,62 @@
 import React from 'react'
 import { View, Text } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { TextInput, Button } from 'react-native-paper'
 const LoginScreen = () => {
-return (
-<View>
-<Text>LoginScreen</Text>
-<h1>Krittipong Wachirangkul</h1>
-<h2>ง่ายจังเลยครับ</h2>
-<h3>11/8/64</h3>
-</View>
-)
+  const { control, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+  return (
+    <View>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            label="Email"
+            mode="outlined"
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="email"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      <View>
+        {errors.email && <Text>You must fill in your email</Text>}
+      </View>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            label="Password"
+            mode="outlined"
+            secureTextEntry
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="password"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      <View>
+        {errors.password && <Text>You must fill in your password</Text>}
+      </View>
+      <View>
+        <Button
+          mode="contained"
+          compact={false}
+          onPress={handleSubmit(onSubmit)}
+          icon="account-arrow-right"
+        >
+          Sign in
+        </Button>
+      </View>
+    </View>
+  )
 };
 export default LoginScreen;
